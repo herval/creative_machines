6997b0802a80dc25d118ad91aa2ef4cd483466b3
herval <hervalfreire@gmail.com>
2016-10-10 21:09:48 -0700

    somewhat trained dictionary

--- dictiowat/src/main/kotlin/hervalicious/dictiowat/BotRunner.kt
+++ dictiowat/src/main/kotlin/hervalicious/dictiowat/BotRunner.kt
@@ -8,20 +8,18 @@ import hervalicious.twitter.Bot
 /**
  * Created by herval on 10/31/15.
  */
-class BotRunner {
+object BotRunner {
 
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            val config = Config()
+    @JvmStatic fun main(args: Array<String>) {
+        val config = Config()
 
-            val bot = Bot(
-                    config,
-                    ArticleWriter.build(config)
-            )
+        val bot = Bot(
+                config,
+                ArticleWriter.build(config)
+        )
 
-            val proc = Thread(bot)
-            proc.run()
-            proc.join()
-        }
+        val proc = Thread(bot)
+        proc.run()
+        proc.join()
     }
 }
